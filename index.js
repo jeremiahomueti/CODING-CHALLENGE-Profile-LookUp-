9bf33ba31a1b52bc588af3c0576ec5f061b7018c
jeremiahomueti <jeremiahomueti@gmail.com>
2022-05-19 23:16:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//CODING CHALLENGE[Profile Lookup]
+
+var contacts = [
+{
+"firstName": "Akira", 
+"lastName": "Laine",
+"number": "0923929389",
+"likes": ["Pizza", "Coding", "Brownie Points"]
+},
+
+{
+"firstName": "Harry", 
+"lastName": "Potter",
+"number": "0994329889",
+"likes": ["Hogwarts", "Magic", "Hagrid"]
+},
+
+{
+"firstName": "Sherlock", 
+"lastName": "Holmes",
+"number": "048739989",
+"likes": ["Intriguing Cases", "Violin"]
+},
+
+{
+"firstName": "Kristian", 
+"lastName": "Vos",
+"number": "Unknown",
+"likes": ["JavaScript", "Gaming", "Doves"]
+},
+
+];
+
+function lookUpProfile(name, prop)
+{
+
+    
+}
